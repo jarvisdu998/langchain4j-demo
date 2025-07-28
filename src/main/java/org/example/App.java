7ad62d4ff78825c323bc7f3f7b2fce7a11cf96bd
ikun009 <1742201@qq.com>
2025-07-28 22:34:06 +0800

    hello

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        OpenAiChatModel.builder()
+                .baseUrl("https://api.openai.com/v1")
+                .apiKey("sk-xxx")
+                .build();
+    }
+}
